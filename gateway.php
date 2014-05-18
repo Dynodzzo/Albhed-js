780483c61b066a3f010cf8ebe824402fa2c37378
Dynodzzo <dynodzoo@gmail.com>
2014-05-18 18:46:59 +0200

    PHP gateway enhanced

--- gateway.php
+++ gateway.php
@@ -1,14 +1,12 @@
 <?php
 
-// $GoogleTTSParams = htmlspecialchars($_GET['ie']);
-
-$qs = http_build_query(array("ie" => "utf-8","tl" => $_GET["tl"], "q" => $_GET["q"]));
-$ctx = stream_context_create(array("http"=>array("method"=>"GET","header"=>"Referer: \r\n")));
-$soundfile = file_get_contents("http://translate.google.com/translate_tts?".$qs, false, $ctx);
+$queryString = http_build_query(array('ie' => 'utf-8', 'tl' => htmlspecialchars($_GET['tl']), 'q' => htmlspecialchars($_GET['q'])));
+$context = stream_context_create(array('http' => array('method' => 'GET', 'header' => "Referer: \r\n")));
+$soundFile = file_get_contents('http://translate.google.com/translate_tts?' . $queryString, false, $context);
  
 header("Content-type: audio/mpeg");
 header("Content-Transfer-Encoding: binary");
 header('Pragma: no-cache');
 header('Expires: 0');
  
-echo($soundfile);
+echo($soundFile);
